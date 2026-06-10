0bbbaf29b1170ca9e1c9a8a740b06216a7e14452
LePhuocTri2205 <lephuoctri2205@gmail.com>
2026-06-10 12:00:00 +0700

    setup folder structure and api utilitie

--- App.tsx
+++ App.tsx
@@ -1,1 +1,14 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "@/global.css";
+
+export default function App() {
+  return (
+    <GluestackUIProvider mode="light">
+      <SafeAreaProvider>
+
+      </SafeAreaProvider>
+    </GluestackUIProvider>
+  );
+}
